ffed99ec00bd73dd5fd0441d38e476a8d7b56d58
Mut Tulow <muttulow@Muts-MBPro.local>
2022-09-25 16:23:45 -0600

    Auto Updated: Sun 23:16 Sep 25 2022

--- website/blog/models.py
+++ website/blog/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+
 
 # Create your models here.
+
+STATUS = (
+    (0,"Draft"),
+    (1,"Publish")
+)
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete= models.CASCADE,related_name='blog_posts')
+    updated_on = models.DateTimeField(auto_now= True)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    def get_absolute_url(self):
+        return reverse('blog_post_detail', args=[self.slug])
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super(Post, self).save(*args, **kwargs)
+
+    class Meta:
+        ordering = ['created_on']
+
+        def __unicode__(self):
+            return self.title
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    name = models.CharField(max_length=42)
+    email = models.EmailField(max_length=75)
+    website = models.URLField(max_length=200, null=True, blank=True)
+    content = models.TextField()
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    created_on = models.DateTimeField(auto_now_add=True)
