2fd81ffb6ccd4032cabe65f02b08ad60f46b7cca
Mut Tulow <muttulow@Muts-MBPro.local>
2022-09-25 18:58:17 -0600

    Auto Updated: Sun 58:18 Sep 25 2022

--- website/blog/urls.py
+++ website/blog/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-
 from . import views
 
+
 urlpatterns = [
-    path('', views.index, name='index'),
+    # path('', views.index, name='index'),
+    path('', views.PostList.as_view(), name='home'),
+    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('posts/', views.posts, name='posts'),
     path('comments/', views.comments, name='comments'),
 ]
--- website/blog/views.py
+++ website/blog/views.py
@@ -1,14 +1,36 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import generic
+from .models import Post
 
 
 # Create your views here.
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the blog index.")
+# ===
+# Function views
+# ===
+
+# def index(request):
+#     return HttpResponse("Hello, world. You're at the blog index.")
 
 def posts(request):
     return HttpResponse("Hello, world. You're at the blog posts.")
 
 def comments(request):
     return HttpResponse("Hello, world. You're at the blog comments.")
+
+
+
+# ===
+# Function views
+# ===
+
+class PostList(generic.ListView):
+    queryset = Post.objects.filter(status=1).order_by('created_on')
+    template_name = 'index.html'
+
+    
+
+class PostDetail(generic.DetailView):
+    model = Post
+    template_name = 'post_detail.html'
